2d22dea1ff6a8c9114002b8ea76334161c033417
Sherif Elfadaly <sh.elfadaly@gmail.com>
2020-02-20 15:17:51 +0200

    phpcs xml copy

--- src/ApiSkeletonServiceProvider.php
+++ src/ApiSkeletonServiceProvider.php
@@ -22,7 +22,7 @@ class ApiSkeletonServiceProvider extends ServiceProvider
             __DIR__.'/../files/BroadcastServiceProvider.php' => app_path('Providers/BroadcastServiceProvider.php'),
             __DIR__.'/../files/Kernel.php'                   => app_path('Console/Kernel.php'),
             __DIR__.'/../files/channels.php'                 => app_path('routes/channels.php'),
-            __DIR__.'/../phpcs.xml'                               => base_path(''),
+            __DIR__.'/../phpcs.xml'                          => base_path('/phpcs.xml'),
         ]);
 
         $this->publishes([
